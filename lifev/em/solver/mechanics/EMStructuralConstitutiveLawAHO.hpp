f5664c57f2a86a65b9bc13dd7ca0f21d13d55c9e
thomaskummer <kummetho@gmail.com>
2019-10-30 14:40:50 +0100

    euler lifev update

--- lifev/em/solver/mechanics/EMStructuralConstitutiveLawAHO.hpp
+++ lifev/em/solver/mechanics/EMStructuralConstitutiveLawAHO.hpp
@@ -1006,7 +1006,7 @@ protected:
         NonlinearMaterial() {}
         ~NonlinearMaterial() {}
         
-        virtual return_Type operator() (const std::vector<MatrixSmall<3,3> >& matrices, const std::vector<VectorSmall<3> >& vectors, const Real& g) = 0;
+        virtual return_Type operator() (const std::vector<MatrixSmall<3,3> >& matrices, const std::vector<VectorSmall<3> >& vectors, const Real& g) {};
         
         MatrixSmall<3,3> deformationGradient (const LifeV::MatrixSmall<3,3>& du) const
         {
