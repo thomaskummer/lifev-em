f0ce555b0272a3f90eb96bd3b50d8c920d162eb4
thomaskummer <kummetho@gmail.com>
2020-05-07 19:42:25 +0200

    euler lifev update

--- lifev/em/solver/EMSolver.hpp
+++ lifev/em/solver/EMSolver.hpp
@@ -1073,6 +1073,9 @@ EMSolver<Mesh, ElectroSolver>::computeDeformedFiberDirection (VectorEpetra& f_, 
     dUdy = GradientRecovery::ZZGradient (feSpacePtr, disp, 1);
     dUdz = GradientRecovery::ZZGradient (feSpacePtr, disp, 2);
     
+    f_ = dUdx;
+    return;
+    
     int n = f_.epetraVector().MyLength() / 3;
     int i (0); int j (0); int k (0);
     MatrixSmall<3,3> F; VectorSmall<3> f0;
